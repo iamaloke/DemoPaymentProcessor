19a94944f7faa0a514b3d044899d455b6dabd59b
Alok Kumar <alok.kumar@ovyo.com>
2025-04-16 14:21:57 +0530

    Initial Commit

---
+++ DemoPaymentProcessor/DemoPaymentProcessorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoPaymentProcessorApp.swift
+//  DemoPaymentProcessor
+//
+//  Created by Alok Kumar on 16/04/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoPaymentProcessorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
